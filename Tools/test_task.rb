f3516f25eb2be501a33ecad8b1500f210f7ad58f
mocchi0420 <mocchi.0420@gmail.com>
2018-01-24 09:53:28 +0900

    reworked

--- Tools/test_task.rb
+++ Tools/test_task.rb
@@ -1,17 +1,8 @@
 require "./DataTableFactory.rb"
 
-CURRENT_PATH = File.expand_path(File.dirname(__FILE__))
-CSV_PATH = CURRENT_PATH+"/csv"
-MARSHAL_PATH = CURRENT_PATH+"/marshal"
-
-
 MYGAME_PATH = File.expand_path("..")
 MASTER_PATH = MYGAME_PATH+"/Data/Master"
 OUTPUT_PATH = MYGAME_PATH+"/Data/output"
-
-
-
-
 
 Dir.glob(MASTER_PATH+"/**/*.*").each do |file_name|
 	DataTableFactory.dump(file_name, OUTPUT_PATH)
