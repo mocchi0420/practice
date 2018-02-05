bbe95b2ae15927ebb474f55de866f9108035517e
mocchi0420 <mocchi.0420@gmail.com>
2018-02-05 10:02:56 +0900

    hogehoge

--- hoge.rb
+++ hoge.rb
@@ -1,44 +1,67 @@
 class Game
+	GAME_FRAME = 9
+	PIN_NUM = 10
 	def initialize
-		@result = Array.new(9).map{Array.new(2,0)}
+		@result = Array.new(GAME_FRAME).map{Array.new(2,0)}
 		@result << [0,0,0]
-		
+		@strike_sheet = Array.new(10,nil)
 		@score = []
+		(GAME_FRAME+1).times do
+			@score << {point: 0, calculated: false}
+		end
+		
 		@frame = 0
 		@throwing_num = 0
 	end
 
 	def roll(pins)
-		return false if pins.to_s !~ /\A[0-9]+\z/ || pins > 10
-		if @frame < 9
-			return false if (@result[@frame].inject(:+) + pins) > 10
-			flag = (@result[@frame].inject(:+) + pins == 10) ? true : false
-			self.calc_result(pins, flag)
-		elsif @frame == 9
-			return false if @result[@frame].select{|e| e > 10} != []
-			case @throwing_num
-				when 0 then flag = (pins == 10) ? true : false
-				when 1 then flag = (@result[@frame][0] + pins == 10 || pins == 10) ? true : false
-				when 2 then flag = ( (@result[@frame][0] == 10 && @result[@frame][1] + pins == 10) || pins == 10) ? true : false
-				else flag = false
-			end
-			self.calc_result(pins, flag)
+		return false if pins.to_s !~ /\A[0-9]+\z/ || pins > PIN_NUM
+		case @frame
+			when 0..(GAME_FRAME-1) then return self.roll_at_normalframe(pins)
+			when GAME_FRAME then return self.roll_at_lastframe(pins)
+			else return false
 		end
+		return true
+	end
+	
+	def roll_at_normalframe(pins)
+		return false if (@result[@frame].inject(:+) + pins) > PIN_NUM
+		flag = (@result[@frame].inject(:+) + pins == PIN_NUM) ? true : false
+		self.calc_result(pins, flag)
+		return true
+	end
+	
+	def roll_at_lastframe(pins)
+		return false if @result[@frame].select{|e| e > PIN_NUM} != []
+		case @throwing_num
+			when 0 then flag = (pins == PIN_NUM) ? true : false
+			when 1 then flag = (@result[@frame][0] + pins == PIN_NUM || pins == PIN_NUM) ? true : false
+			when 2 then flag = ( (@result[@frame][0] == PIN_NUM && @result[@frame][1] + pins == PIN_NUM) || pins == PIN_NUM) ? true : false
+			else flag = false
+		end
+		self.calc_result(pins, flag)
 		return true
 	end
 
 	def calc_result(pins, ten_pins=false)
-		return calc_result_lastround(pins, ten_pins) if @frame >= 9
+		case @frame
+			when 0..(GAME_FRAME-1) then return self.calc_result_normalframe(pins, ten_pins)
+			when GAME_FRAME then return self.calc_result_lastframe(pins, ten_pins)
+			else return false
+		end
+	end
+	
+	def calc_result_normalframe(pins, ten_pins=false)
 		@result[@frame][@throwing_num] += pins
 		if ten_pins == true
-			tmp = (@throwing_num == 0) ? "STRIKE" : "SPARE"
-			@score << tmp
+			@strike_sheet[@frame] = (@throwing_num == 0) ? "STRIKE" : "SPARE"
 			@frame += 1
 			@throwing_num = 0
 			return true
 		end
 		if @throwing_num >= 1
-			@score << @result[@frame].inject(:+)
+			@score[@frame][:point] = @result[@frame].inject(:+)
+			@score[@frame][:calculated] = true
 			@frame += 1
 			@throwing_num = 0
 			return true
@@ -46,21 +69,17 @@ class Game
 		@throwing_num += 1
 		return true
 	end
-	
-	def calc_result_lastround(pins, ten_pins=false)
-		return false if @frame > 9
+
+	def calc_result_lastframe(pins, ten_pins=false)
+		return false if @frame > GAME_FRAME
 		@result[@frame][@throwing_num] += pins
 		case @throwing_num
 			when 0,2
-				@score << "STRIKE" if pins == 10 && ten_pins == true
+				@strike_sheet[@frame] = "STRIKE" if pins == PIN_NUM && ten_pins == true
 				@throwing_num += 1
 			when 1
 				if ten_pins == true
-					if pins == 10
-						@score << "STRIKE" 
-					else
-						@score << "SPARE"
-					end
+					@strike_sheet[@frame] = (pins == PIN_NUM) ? "STRIKE" : "SPARE"
 					@throwing_num += 1
 				else
 					@throwing_num += 2
@@ -72,30 +91,83 @@ class Game
 	end
 	
 	def score
-		return 0 if @score == []
+		tmp = 0
 		@score.each_with_index do |item, index|
-			if index > (@score.length - 2) - 1
-				@score[index] = 0
-			else
-				@score[index] = calc_point(index, item) if item == "STRIKE" || item == "SPARE"
-			end
+			calc_point(index, item) if item[:calculated] != true
+			tmp += @score[index][:point] if item[:calculated] == true
 		end
-		return @score.inject(:+)
+		return tmp
 	end
 	
 	def calc_point(index, item)
-		if item == "STRIKE" then
-			if @score[index+1] != "STRIKE"
-				return @result[index].inject(:+) + @result[index+1].inject(:+)
-			elsif @score[index+1] == "STRIKE" && @score[index+2] != "STRIKE"
-				return @result[index].inject(:+) + @result[index+1].inject(:+) + @result[index+2][0]
-			elsif @score[index+1] == "STRIKE" && @score[index+2] == "STRIKE"
-				return 30
-			end
-		elsif item == "SPARE" then
-			return @result[index].inject(:+) + @result[index+1][0]
+		case index
+			when 0..7
+				if @strike_sheet[index] == "STRIKE" then
+					if @strike_sheet[index+1] != "STRIKE"
+						@score[index][:point] = @result[index].inject(:+) + @result[index+1].inject(:+)
+						@score[index][:calculated] = true
+					elsif @strike_sheet[index+1] == "STRIKE" && @strike_sheet[index+2] != "STRIKE"
+						@score[index][:point] = @result[index].inject(:+) + @result[index+1].inject(:+) + @result[index+2][0]
+						@score[index][:calculated] = true
+					elsif @strike_sheet[index+1] == "STRIKE" && @strike_sheet[index+2] == "STRIKE"
+						@score[index][:point] = 30
+						@score[index][:calculated] = true
+					end
+				elsif @strike_sheet[index] == "SPARE" then
+					@score[index][:point] = @result[index].inject(:+) + @result[index+1][0]
+					@score[index][:calculated] = true
+				end
+			when 8
+				if @strike_sheet[index] == "STRIKE" then
+					if @strike_sheet[index+1] != "STRIKE"
+						@score[index][:point] = @result[index].inject(:+) + @result[index+1].inject(:+)
+						@score[index][:calculated] = true
+					elsif @strike_sheet[index+1] == "STRIKE"
+						@score[index][:point] = @result[index].inject(:+) + @result[index+1].inject(:+)
+						@score[index][:calculated] = true
+					elsif @strike_sheet[index+1] == "STRIKE" && @strike_sheet[index+2] == "STRIKE"
+						@score[index][:point] = 30
+						@score[index][:calculated] = true
+					end
+				elsif @strike_sheet[index] == "SPARE" then
+					@score[index][:point] = @result[index].inject(:+) + @result[index+1][0]
+					@score[index][:calculated] = true
+				end
+			when 9
+			
 		end
 	
+	
+	
+	
+	
+	
+	
+	
+	
+		#return 0 if @frame == index+1
+		if index < GAME_FRAME
+			if @strike_sheet[index] == "STRIKE" || index == GAME_FRAME-1 then
+				if @strike_sheet[index+1] != "STRIKE"
+					@score[index][:point] = @result[index].inject(:+) + @result[index+1].inject(:+)
+					@score[index][:calculated] = true
+				elsif @strike_sheet[index+1] == "STRIKE" && @strike_sheet[index+2] != "STRIKE" && index < GAME_FRAME-1
+					@score[index][:point] = @result[index].inject(:+) + @result[index+1].inject(:+) + @result[index+2][0]
+					@score[index][:calculated] = true
+				elsif @strike_sheet[index+1] == "STRIKE" && @strike_sheet[index+2] == "STRIKE" && index < GAME_FRAME-1
+					@score[index][:point] = 30
+					@score[index][:calculated] = true
+				end
+			elsif @strike_sheet[index] == "SPARE" then
+				@score[index][:point] = @result[index].inject(:+) + @result[index+1][0]
+				@score[index][:calculated] = true
+			end
+		elsif index == GAME_FRAME
+			@score[index][:point] = @result.last.inject(:+)
+			@score[index][:calculated] = true
+		else
+
+		end
 	end
 	
 	def reset
--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -1,128 +1,153 @@
 require_relative '../hoge'
 
 describe "Bowling game" do
-	mygame = Game.new
-	it "we can check 0 score" do
-		mygame.reset
-		expect(mygame.score).to eq(0)
-	end
-	it "we can check 0 frame" do
-		mygame.reset
-		expect(mygame.instance_exec{@frame}).to eq(0)
-	end
-	
-	it "we can defeat 0 pins once" do
-		mygame.reset
-		expect(mygame.roll(0)).to eq(true)
-	end
-	
-	it "we can defeat 10 pins once" do
-		mygame.reset
-		expect(mygame.roll(10)).to eq(true)
-	end
-	
-	it "we cannot defeat -1 pins once" do
-		mygame.reset
-		expect(mygame.roll(-1)).to eq(false)
-	end
-	
-	it "we cannot defeat 11 pins once" do
-		mygame.reset
-		expect(mygame.roll(11)).to eq(false)
-	end
-	
-	it "we cannot defeat str pins once" do
-		mygame.reset
-		expect(mygame.roll("hogehoge")).to eq(false)
-	end
-
-	it "we cannot defeat 0.3 pins once" do
-		mygame.reset
-		expect(mygame.roll(0.3)).to eq(false)
-	end
-
-	it "we can defeat total 10 pins" do
-		mygame.reset
-		mygame.roll(7)
-		expect(mygame.roll(3)).to eq(true)
-		mygame.roll(0)
-		expect(mygame.roll(10)).to eq(true)
-		expect(mygame.roll(10)).to eq(true)
-	end
-
-	it "we cannot defeat total 10 over pins" do
-		mygame.reset
-		mygame.roll(7)
-		expect(mygame.roll(4)).to eq(false)
-		mygame.reset
-		mygame.roll(0)
-		expect(mygame.roll(11)).to eq(false)
-	end
-
-	it "we must go next frame when we throw 2 ball" do
-		mygame.reset
-		mygame.roll(1)
-		mygame.roll(1)
-		expect(mygame.instance_exec{@frame}).to eq(1)
-	end
-	
-	it "we must get 0 point when we defeat 0 pins" do
-		mygame.reset
-		mygame.roll(0)
-		mygame.roll(0)
-		expect(mygame.score).to eq(0)
-	end
-	
-	it "we must get STRIKE point when we defeat 10 pins ONCE" do
-		mygame.reset
-		mygame.roll(10)
-		expect(mygame.instance_exec{@result[0]}).to eq([10,0])		
-		expect(mygame.instance_exec{@score[0]}).to eq("STRIKE")
-	end
-	
-	it "we must get SPARE point when we defeat 10 pins" do
-		mygame.reset
-		mygame.roll(0)		
-		mygame.roll(10)
-		expect(mygame.instance_exec{@result[0]}).to eq([0,10])
-		expect(mygame.instance_exec{@score[0]}).to eq("SPARE")
-	end
-	
-	it "we get ALL_STRIKE" do
-		mygame.reset
-		12.times do
-			mygame.roll(10)
+	before do
+		@mygame = Game.new
+	end	
+	describe "game start" do
+		before do
+			#nothing to do
+		end	
+		
+		it "we can check 0 score" do
+			expect(@mygame.score).to eq(0)
 		end
-		expect(mygame.instance_exec{@result}).to eq([[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]])
-		expect(mygame.instance_exec{@score}).to eq(["STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE","STRIKE"])
-		expect(mygame.score).to eq(300)
+		
+		it "we can check 0 frame" do
+			expect(@mygame.instance_exec{@frame}).to eq(0)
+		end
 	end
 	
-	it "we get 9 score when 6pins + 3pins at 1 frame" do
-		mygame.reset
-		mygame.roll(6)
-		mygame.roll(3)
-		expect(mygame.instance_exec{@result[0]}).to eq([6,3])
-		expect(mygame.instance_exec{@score[0]}).to eq(9)
+	describe "defeat pins " do
+		context "nominal scenarios" do
+			before do
+				#nothing to do
+			end	
+			it "we can defeat 0 pins once" do
+				expect(@mygame.roll(0)).to eq(true)
+			end
+			
+			it "we can defeat 10 pins once" do
+				expect(@mygame.roll(10)).to eq(true)
+			end
+			
+			it "we can defeat total 10 pins" do
+				@mygame.roll(7)
+				expect(@mygame.roll(3)).to eq(true)
+				@mygame.roll(0)
+				expect(@mygame.roll(10)).to eq(true)
+				expect(@mygame.roll(10)).to eq(true)
+			end
+		end
+		
+		context "non-nominal scenarios" do
+			before do
+				#nothing to do
+			end
+			
+			it "we cannot defeat -1 pins once" do
+				expect(@mygame.roll(-1)).to eq(false)
+			end
+			
+			it "we cannot defeat 11 pins once" do
+				expect(@mygame.roll(11)).to eq(false)
+			end
+			
+			it "we cannot defeat str pins once" do
+				expect(@mygame.roll("hogehoge")).to eq(false)
+			end
+
+			it "we cannot defeat 0.3 pins once" do
+				expect(@mygame.roll(0.3)).to eq(false)
+			end
+			
+			it "we cannot defeat total 10 over pins" do
+				@mygame.roll(7)
+				expect(@mygame.roll(4)).to eq(false)
+				@mygame.reset
+				@mygame.roll(0)
+				expect(@mygame.roll(11)).to eq(false)
+			end
+		end
 	end
 	
-	it "we get 9 score when 6pins + 3pins at 1 frame" do
-		mygame.reset
-		mygame.roll(6)
-		mygame.roll(3)
-		expect(mygame.instance_exec{@result[0]}).to eq([6,3])
-		expect(mygame.instance_exec{@score[0]}).to eq(9)
-		expect(mygame.score).to eq(9)
+	describe "play normal game" do
+		before do
+			#nothing to do
+		end
+		describe "at 0-9 frame" do
+			it "we must go next frame when we throw 2 ball" do
+				@mygame.roll(1)
+				@mygame.roll(1)
+				expect(@mygame.instance_exec{@frame}).to eq(1)
+			end
+			
+			it "we must get 0 point when we defeat 0 pins" do
+				@mygame.roll(0)
+				@mygame.roll(0)
+				expect(@mygame.score).to eq(0)
+			end
+			
+			it "we must get STRIKE point when we defeat 10 pins ONCE" do
+				@mygame.roll(10)
+				expect(@mygame.instance_exec{@result[0]}).to eq([10,0])		
+				expect(@mygame.instance_exec{@strike_sheet[0]}).to eq("STRIKE")
+			end
+			
+			it "we must get SPARE point when we defeat 10 pins" do
+				@mygame.roll(0)		
+				@mygame.roll(10)
+				expect(@mygame.instance_exec{@result[0]}).to eq([0,10])
+				expect(@mygame.instance_exec{@strike_sheet[0]}).to eq("SPARE")
+			end
+			
+			it "we get 9 score when 6pins + 3pins at 1 frame" do
+				@mygame.roll(6)
+				@mygame.roll(3)
+				expect(@mygame.instance_exec{@result[0]}).to eq([6,3])
+				expect(@mygame.score).to eq(9)
+			end
+		
+		end
+		
+		describe "at 10 frame" do
+		
+		end
+		
+		describe "through 1 set game" do
+			it "we get ALL_STRIKE" do
+				12.times do
+					@mygame.roll(10)
+				end
+				
+				@mygame.score
+				@mygame.instance_exec{p @score}
+				expect(@mygame.instance_exec{@result}).to eq([[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]])
+				expect(@mygame.score).to eq(300)
+			end
+			
+			it "we get right score as example" do
+				@mygame.roll(6)
+				@mygame.roll(3)
+				expect(@mygame.score).to eq(9)
+				@mygame.roll(9)
+				@mygame.roll(0)
+				expect(@mygame.score).to eq(18)
+				@mygame.roll(0)
+				@mygame.roll(3)
+				expect(@mygame.score).to eq(21)
+				@mygame.roll(8)
+				@mygame.roll(2)
+				expect(@mygame.score).to eq(21)
+				@mygame.roll(7)
+				@mygame.roll(3)
+				expect(@mygame.score).to eq(38)
+				@mygame.roll(10)
+				expect(@mygame.score).to eq(58)
+			end
+			
+		end
+
 	end
-	
-	it "we get right score as example" do
-		mygame.reset
-		mygame.roll(6)
-		mygame.roll(3)
-		mygame.roll(9)
-		mygame.roll(0)
-		expect(mygame.score).to eq(18)
-	end
-	
 	
 end
