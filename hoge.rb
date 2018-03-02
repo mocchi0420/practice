5dc06b4b2be5493e26d46f0793426e18aa25c3d7
suzuki motoharu <suzuki@toydea.com>
2018-03-02 12:23:31 +0900

    KATA completed

--- hoge.rb
+++ hoge.rb
@@ -6,11 +6,25 @@ class Game
 		@current_result = []
 		@score = []
 		@frame = 0
+		@strike_count = 0
+	end
+
+	def roll_check(pins)
+		return false if @frame > GAME_FRAME
+		return false if pins.to_s !~ /\A[0-9]+\z/ || pins > PIN_NUM
+		
+		if @frame < GAME_FRAME
+			return false if (@current_result.length >= 1) && @current_result.inject(:+) + pins > 10
+		else
+			return false if (@current_result.length >= 1) && (@current_result[0] + pins > 20)
+			return false if (@current_result.length >= 2) && (@current_result[0] + @current_result[1] + pins > 30)
+		end
+		return true
 	end
 
 	def roll(pins)
-		return false if @frame > GAME_FRAME
-		return false if pins.to_s !~ /\A[0-9]+\z/ || pins > PIN_NUM
+		return false if roll_check(pins) == false
+
 		@current_result << pins
 		
 		if @frame < GAME_FRAME
@@ -26,14 +40,33 @@ class Game
 			@current_result = []
 			@frame += 1
 		end
+		
 		return true
 	end
 	
-	def get_score
+	def score
 		return 0 if @frame == 0
 		(@frame-1).downto(0) do |idx|
-			@score.unshift(@result[idx]).flatten! if idx == 9
-			@score.unshift(@result[idx].inject(:+)) if idx != 9
+			if idx == 9
+				@score.unshift(@result[idx]).flatten!
+				next
+			end
+			
+			ret = 0			
+			if @result[idx] == [10,0]
+				case @strike_count
+					when 0 then ret = (@frame - idx > 1) ? (@result[idx+1][0..1].inject(:+) + @result[idx].inject(:+)) : 0
+					when 1 then ret = (@frame - idx > 2) ? (@result[idx+2][0] + @result[idx..(idx+1)].flatten!.inject(:+)) : 0
+					when 2 then ret = (@frame - idx > 2) ? 30 : 0
+				end
+				@strike_count += 1 if @strike_count < 2
+			elsif @result[idx].inject(:+) == 10
+				 ret = (@frame != idx) ? (@result[idx+1][0]+@result[idx].inject(:+)) : 0
+			else
+				 ret = @result[idx].inject(:+)
+			end
+			@strike_count = 0 if @result[idx] != [10,0]
+			@score.unshift(ret)
 		end
 		return @score.inject(:+)
 	end
--- spec/hoge_spec.rb
+++ spec/hoge_spec.rb
@@ -42,6 +42,18 @@ describe "Bowling game" do
 			expect(@mygame.instance_exec{@frame}).to eq(1)
 		end
 		
+		it "we cannot get over 10 pins at 1 throw" do
+			expect(@mygame.roll(11)).to eq(false)
+		end
+		
+		it "we cannot get over 10 pins at 1 round" do
+			@mygame.roll(3)
+			@mygame.roll(8)
+			expect(@mygame.instance_exec{@current_result}).to eq([3])
+			expect(@mygame.instance_exec{@result}).to eq([])
+			expect(@mygame.instance_exec{@frame}).to eq(0)
+		end
+		
 		it "we can get right 20 throws getting 1pin at 1game" do
 			20.times do
 				@mygame.roll(1)
@@ -100,13 +112,13 @@ describe "Bowling game" do
 		
 		it "we can get 0 pts at 1st throw" do
 			@mygame.roll(6)
-			expect(@mygame.get_score).to eq(0)
+			expect(@mygame.score).to eq(0)
 		end
 		
-		it "we can get right pts at 2st throw" do
+		it "we can get right pts at 2nd throw" do
 			@mygame.roll(6)
 			@mygame.roll(1)
-			expect(@mygame.get_score).to eq(7)
+			expect(@mygame.score).to eq(7)
 		end
 		
 		it "we can get right pts at 10th round" do
@@ -116,12 +128,56 @@ describe "Bowling game" do
 			@mygame.roll(10)
 			@mygame.roll(10)
 			@mygame.roll(10)
-			ret = @mygame.get_score
+			ret = @mygame.score
 			expect(@mygame.instance_exec{@result}).to eq([[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[10,10,10]])
 			expect(@mygame.instance_exec{@score}).to eq([0,0,0,0,0,0,0,0,0,10,10,10])
 			expect(ret).to eq(30)
 		end
 		
+		it "we can get 300 pts at 12 strikes" do
+			12.times do
+				@mygame.roll(10)
+			end
+			ret = @mygame.score
+			expect(@mygame.instance_exec{@result}).to eq([[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]])
+			expect(@mygame.instance_exec{@score}).to eq([30,30,30,30,30,30,30,30,30,10,10,10])
+			expect(ret).to eq(300)
+		end
+		
+		it "we cannot confirm pts at all strikes" do
+			9.times do
+				@mygame.roll(10)
+			end
+			ret = @mygame.score
+			expect(@mygame.instance_exec{@result}).to eq([[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0]])
+			expect(@mygame.instance_exec{@score}).to eq([30,30,30,30,30,30,30,0,0])
+			expect(ret).to eq(210)
+		end
+		
+		it "we can get right result at testgame" do
+			@mygame.roll(6)
+			@mygame.roll(3)
+			@mygame.roll(9)
+			@mygame.roll(0)
+			@mygame.roll(0)
+			@mygame.roll(3)
+			@mygame.roll(8)
+			@mygame.roll(2)
+			@mygame.roll(7)
+			@mygame.roll(3)
+			@mygame.roll(10)
+			@mygame.roll(9)
+			@mygame.roll(1)
+			@mygame.roll(8)
+			@mygame.roll(0)
+			@mygame.roll(10)
+			@mygame.roll(10)
+			@mygame.roll(6)
+			@mygame.roll(4)
+			ret = @mygame.score
+			expect(@mygame.instance_exec{@result}).to eq([[6,3],[9,0],[0,3],[8,2],[7,3],[10,0],[9,1],[8,0],[10,0],[10,6,4]])
+			expect(ret).to eq(150)
+		end
 		
 	end
 	
