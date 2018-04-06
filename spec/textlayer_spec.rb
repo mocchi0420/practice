739b2f2b1c5958a6efcf0571a61fca7bd1c917d2
suzuki motoharu <suzuki@toydea.com>
2018-04-06 18:01:27 +0900

    シナリオエンジン側のクラス構造の模索

--- spec/textlayer_spec.rb
+++ spec/textlayer_spec.rb
@@ -14,8 +14,9 @@ describe "TextBox" do
 				font = Font.new(24, "ＭＳ ゴシック")
 				text = "あいうえおかきくけこ"
 			image = Image.new(640, 480, [0, 0, 0, 0])
-				tb = TextBoxProto2.new(image)
-				tb.set_text(text, font)
+				tb = TextBox.new(image)
+				tb.set_text(text)
+				tb.set_font(font)
 				tb.set_window(100,100,window)
 				tb.draw
 			end).to be_truthy
@@ -25,11 +26,13 @@ describe "TextBox" do
 			font = Font.new(24, "ＭＳ ゴシック")
 			text = "あいうえおかきくけこ"
 			image = Image.new(640, 480, [0, 0, 0, 0])
-			tb = TextBoxProto2.new(image)
-			tb.set_text(text, font)
+			sb = MonoScript.new(image)
+			sb.set_text(text)
+			sb.set_font(font)
+			ms ||= ScriptController.new
+			ms.set_script(sb)
 			expect(Testdriver.mainloop do |window|
-				tb.set_window(100,100,window)
-				ms ||= MonoScript.new(tb)
+				ms.on_draw(100,100,window)
 				ms.on_move
 			end).to be_truthy
 		end
--- textlayer.rb
+++ textlayer.rb
@@ -1,63 +1,69 @@
 # encoding: utf-8
+require 'dxruby'
 
-
-class MonoScript
-	require 'dxruby'
-
-	def initialize(textbox, config={wait_counter_preset: 5, status: :draw, skip_flag: false})
-		@textbox = textbox
+class ScriptController
+	DEFAULT_CAPTION_WAIT = 2
+	def initialize(config={})
+		@script = nil
 		@config = config
-		@wait_counter = @config[:wait_counter_preset]
+		@caption_wait = (@config[:caption_wait]==nil) ? DEFAULT_CAPTION_WAIT : @config[:caption_wait]
+		@status = :draw
+		@skip_flag = false
 	end
 	
 	def on_draw(pos_x, pos_y, window)
-		window.draw(pos_x, pos_y, @textbox.image)
+		@script.set_window(pos_x, pos_y, window)
+	end
+	
+	def set_script(script)
+		@script = script
 	end
 	
 	def on_move
 		begin
-			send(@config[:status]) if self.methods.index(@config[:status]) != nil
-		end while @config[:skip_flag] == true && @config[:status] != :end
+			send(@status) if self.methods.index(@status) != nil
+		end while @skip_flag == true && @status != :end
 		return true
 	end
 	
 	def draw
-		unless @textbox.text_end?
-			@textbox.draw_forscript
+		raise if @script == nil
+		unless @script.text_end?
+			@script.draw
 			self.check_skip	#スキップモードの場合の処理
 		else
-			@config[:status] = :end
+			@status = :end
 		end
 	end
 	
 	def check_skip
-		@config[:status] = (@config[:skip_flag] == true) ? :draw : :wait
+		@status = (@skip_flag == true) ? :draw : :wait
 	end
 
 	def wait
-		if @config[:skip_flag] == true
-			@config[:status] = :draw
+		if @skip_flag == true
+			@status = :draw
 			return true
 		end
 		
-		if @wait_counter <= 0 then
-			@wait_counter = @config[:wait_counter_preset]
-			@config[:status] = :draw
+		if @caption_wait <= 0 then
+			@caption_wait = (@config[:caption_wait]==nil) ? DEFAULT_CAPTION_WAIT : @config[:caption_wait]
+			@status = :draw
 		else
-			@wait_counter -= 1
+			@caption_wait -= 1
 		end
 	end
 
 	def get_status
-		return @config[:status]
+		return @status
 	end
 	
 	def line_feed
-		@textbox.line_feed
+		@script.line_feed
 	end
 	
 	def set_skip_mode(bool)
-		@config[:skip_flag] = bool
+		@skip_flag = bool
 	end
 
 end
@@ -66,81 +72,6 @@ end
 
 
 class TextBox
-	def initialize(text, config=nil)
-		@config = self.set_config(config)
-		@text = text
-		@text_counter = 0
-		@str_size = {x: 0, y: 0}
-		
-		@font = Font.new(@config[:font_size], @config[:font_name])
-		
-	end
-	
-	def set_config(config)
-		ret = {
-			font_color: [255, 255, 255],
-			font_name: "ＭＳ ゴシック",
-			font_size: 24,
-			pos: {x: 0,y: 0},
-			line_space_size: 0
-		}
-		config.each{|key| ret[:key] = config[:key] if config[:key] != nil} if config != nil && config.class == Hash
-		return ret
-	end
-
-	def draw(image)
-		str = @text[@text_counter, 1]
-		image.draw_font_ex(@config[:pos][:x], @config[:pos][:y], str , @font)
-		@text_counter += 1
-		@str_size[:x] = (str.bytesize >= 2) ? @font.size : @font.size/2
-		@str_size[:y] = @font.size
-		@config[:pos][:x] += @str_size[:x]
-		return true
-	end
-
-	def text_end? 
-		ret = (@text.length > @text_counter) ? true : false
-		return ret
-	end
-
-	def line_feed
-		@config[:pos][:x] = 0
-		@config[:pos][:y] += @str_size[:y] + @config[:line_space_size]
-	end
-
-end
-
-
-
-
-class TextBoxProto
-	def initialize(window, image, option={})
-		@image = image
-		window.draw(0, 0, @image)
-		@font = nil
-		@text = nil
-		@pos = {x: 0, y: 0}
-		@option = option
-	end
-
-	def set_text(text, font)
-		@text = text
-		@font = font
-	end
-
-	def draw
-		return false if @text == nil || @font == nil
-		@image.draw_font_ex(@pos[:x], @pos[:y], @text , @font, @option)
-		return true
-	end
-	
-	def dispose
-		@image.dispose
-	end
-end
-
-
-class TextBoxProto2
 	require 'dxruby'
 	attr_reader :image
 	
@@ -149,20 +80,21 @@ class TextBoxProto2
 		@font = nil
 		@text = nil
 		@pos = {x: 0, y: 0}
-		
-		@text_counter = 0
-		@pos_diff = {x: 0, y: 0}
-		
 		@option = option
 	end
 
-	def set_text(text, font)
+	def set_text(text)
 		@text = text
-		@font = font
-		@text_counter = 0
-		@pos_diff = {x: 0, y: 0}
 	end
 
+	def set_font(font)
+		@font = font
+	end
+	
+	def set_position(pos)
+		@pos = pos
+	end
+	
 	def set_window(pos_x, pos_y, window)
 		window.draw(pos_x, pos_y,@image)
 	end
@@ -178,11 +110,23 @@ class TextBoxProto2
 	def draw
 		return false if @text == nil || @font == nil
 		@image.draw_font_ex(@pos[:x], @pos[:y], @text , @font, @option)
-		return true
+	end
+end
+
+
+class MonoScript < TextBox
+	def initialize(image, option={})
+		super
+		@text_counter = 0
+		@pos_diff = {x: 0, y: 0}
 	end
 	
-	#以下、monoscriptで呼ばれる場合の形式
-	def draw_forscript
+	def reset_script
+		@text_counter = 0
+		@pos_diff = {x: 0, y: 0}
+	end
+	
+	def draw
 		return false if @text == nil || @font == nil
 		str = @text[@text_counter, 1]
 		@image.draw_font_ex(@pos[:x], @pos[:y], str , @font, @option)
@@ -192,7 +136,7 @@ class TextBoxProto2
 		@pos[:x] += @pos_diff[:x]
 	end
 
-	def text_end? 
+	def text_end?
 		ret = (@text.length > @text_counter) ? false : true
 		return ret
 	end
@@ -207,17 +151,3 @@ end
 
 
 
-font = Font.new(24, "ＭＳ ゴシック")
-image1 = DXRuby::Image.new(640, 480, [0, 0, 0, 0])
-
-Marshal.dump(image1)
-
-puts image1.class.ancestors
-
-Window.loop do
-	image2 = Image.new(640, 480, [0, 0, 0, 0])
-	Window.draw(0, 0,image1)
-	image1.draw_font_ex(0,0,"あいうえおかきくけこ",font,{edge: true})	#オプションでedgeを指定してあげる
-	Window.draw(0, 100,image2)
-	image2.draw_font_ex(0,0,"あいうえおかきくけこ",font)
-end
